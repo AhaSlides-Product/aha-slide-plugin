a8a6f22afed43ef75011d948d699fd92efe1240f
Lily Fancy Nguyen <lily@ahaslides.com>
2026-01-23 10:46:33 +0700

    Setup playwright for SDK & sample tests

---
+++ tests/e2e/audience/audience-view.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+/**
+ * E2E Tests for Audience View
+ * 
+ * Tests the AudienceSlidePluginIframe component and verifies that
+ * all props from AudienceSlidePluginProps interface are correctly passed.
+ */
+test.describe('Audience View', () => {
+ 
+});
