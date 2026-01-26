ced9b7e21b75242fc825363603c0cafbd0a6499d
Tam <tam@ahaslides.com>
2026-01-26 14:05:13 +0700

    wip

--- packages/common/src/eqmx.ts
+++ packages/common/src/eqmx.ts
@@ -1,6 +1,6 @@
 export function getBucket({ presentationId, slideId, slideVersion, name }: {
-  presentationId: string;
-  slideId: string;
+  presentationId: string | number;
+  slideId: string | number;
   slideVersion: number;
   name: string;
 }) {
